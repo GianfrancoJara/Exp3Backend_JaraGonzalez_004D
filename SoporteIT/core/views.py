dfd350707e327c003129d7812b68d18e065735d2
GianfrancoJara <gia.jara@alumnos.duoc.cl>
2021-06-21 21:27:14 -0400

    Inicio de proyecto Django

---
+++ SoporteIT/core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Vehiculo
+
+# Create your views here.
+
+def home(request):
+
+    vehiculos = Vehiculo.objects.all()
+
+    datos = {
+        'vehiculos': vehiculos
+    }
+
+    return render(request, 'home.html', datos)
